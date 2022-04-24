6a3ed7fffb4b02de586c6f5ad777a820a5ae407c
Pablo Trasbach Sánchez <72377040+pablotr21@users.noreply.github.com>
2022-04-24 16:47:55 +0200

    Main

---
+++ proyecto/src/proyecto/Menu.java
@@ -0,0 +1,11 @@
+package proyecto;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			gestorEquiposInt ge = new gestorEquipos();
+			
+	}
+
+}
