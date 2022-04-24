1d441c8dc8f386e1624d4d9f91160f91cb3fc6b0
Pablo Trasbach Sánchez <72377040+pablotr21@users.noreply.github.com>
2022-04-24 09:04:17 +0200

    Finalizada la versión 1.0 de gestor de usuarios (necesidad de revisión en grupo y cambios).

--- proyecto/src/proyecto/gestorUsuarios.java
+++ proyecto/src/proyecto/gestorUsuarios.java
@@ -13,7 +13,9 @@ public class gestorUsuarios implements gestorUsuariosInt{
 	public int registrarUsuario(Usuario usuario) {
 		int result = 0;
 		
+		//Si se introdujo un usuario
 		if(usuario != null) {
+			//Si el usuario introducido no existe en el sistema
 			if(!usuarios.containsKey(usuario.getId())) {
 				usuarios.put(usuario.getId(), usuario);
 				result = 1;
@@ -29,11 +31,14 @@ public class gestorUsuarios implements gestorUsuariosInt{
 	public int darBajaUsuario(String dni) {
 		int result = 0;
 		
+		//Si usuarios no está vacío
 		if(usuarios.isEmpty()) {
 			result = -1;
 		}else {
+			//Se busca el usuario a eliminar por su dni
 			for(Usuario u: usuarios.values()) {
 				if(u.getDni().equals(dni)) {
+					//Se elimina el usuario del sistema
 					usuarios.remove(u.getId());
 					result = 1;
 				}
@@ -45,22 +50,49 @@ public class gestorUsuarios implements gestorUsuariosInt{
 
 	@Override
 	public int declararAlarma(Alarma alarma) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		
+		//Si se introdujo la alarma
+		if(alarma != null) {
+			//Si se envía la alarma correctamente al gestor de alarmas
+			if(recibirAlarma(alarma) == 1) {
+				result = 1;
+			}
+		}else {
+			result = -1;
+		}
+		
+		return result;
 	}
 
 	@Override
 	public int recibirAccionesUsuario(HashMap<Integer, String> acciones) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		
+		//Si se introducen acciones correctamente
+		if(!acciones.isEmpty()) {
+			//Se recorren las acciones introducidas
+			for(Integer i: acciones.keySet()) {
+				//Se envía a cada usuario su acción a realizar
+				System.out.println("El usuario con DNI: " + usuarios.get(i).getDni() + " debe " + acciones.get(i));
+				result = 1;
+			}
+		}else {
+			result = -1;
+		}
+		
+		return result;
 	}
 
 	@Override
 	public int modificarUsuario(Usuario usuario) {
 		int result = 0;
-				
+		
+		//Si se introdujo un usuario
 		if(usuario != null) {
+			//Si el usuario existe en el sistema
 			if(usuarios.containsKey(usuario.getId())) {
+				//Se modifica el usuario
 				usuarios.put(usuario.getId(), usuario);
 				result = 1;
 			}
--- proyecto/src/proyecto/gestorUsuariosInt.java
+++ proyecto/src/proyecto/gestorUsuariosInt.java
@@ -4,10 +4,31 @@ import java.util.HashMap;
 
 public interface gestorUsuariosInt {
     
+	/* Método que introduce un usuario en el sistema.
+	 * Devolverá 1 cuando se introduzca correctamente,
+	 * Devolverá -1 si se le introduce como parámetro null,
+	 * Devolverá 0 si el usuario ya está registrado en el sistema.*/
 	int registrarUsuario(Usuario usuario);
+	
+	/* Método que elimina un usuario del sistema.
+	 * Devolverá 1 cuando se elimine correctamente,
+	 * Devolverá -1 si el sistema no contiene ningún usuario,
+	 * Devolverá 0 si el usuario no existe en el sistema.*/
 	int darBajaUsuario(String dni);
+	
+	/* Método que introduce una alarma en el sistema.
+	 * Devolverá 1 cuando se introduzca correctamente,
+	 * Devolverá -1 si se introduce null como parámetro,
+	 * Devolverá 0 si la alarma ya ha sido introducida anteriormente.*/
 	int declararAlarma(Alarma alarma);
+	
+	/* Método que*/
 	int recibirAccionesUsuario(HashMap<Integer, String> acciones);
+	
+	/* Método que modifica un usuario del sistema.
+	 * Devolverá 1 cuando se modifique correctamente,
+	 * Devolverá -1 si se le introduce null como parámetro,
+	 * Devolverá 0 si el usuario no existe en el sistema.*/
 	int modificarUsuario(Usuario usuario);
 	
 }
